a111c07740eef215973843c917aa1fe1652caaf1
jost2345 <jost2345>
2004-10-28 17:02:55 +0000

    print usage

--- server/java.c
+++ server/java.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
 /* wrapper for the GNU java library */
+void usage(char*name){
+  fprintf(stderr, "Usage: %s <socketname> <logLevel> <logFile>\n", name);
+  fprintf(stderr, "For example: %s /var/run/.php-java-bridge_socket 4 /var/log/php-java-bridge.log\n", name);
+  exit(1);
+}
 main(int argc,char**argv){
   extern void java_bridge_main_gcj(int argc, char**argv);
 
+  if(argc<4) usage(argv[0]);
+
   java_bridge_main_gcj(argc,argv);
+  return 0;
 }
