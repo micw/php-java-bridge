6cdf52d58824c7f23cd96a4130f117e97520aa22
jost2345 <jost2345>
2005-04-14 18:24:37 +0000

    Tests PR1176523

---
+++ tests.php4/Array6.java
@@ -0,0 +1,13 @@
+public class Array6 {
+    public static Object[][][][][][] test() {
+	Object[][][][][][] testobj = new Object[2][2][2][2][2][2];
+        testobj[0][0][0][0][0][1]="1";
+        testobj[0][0][0][0][1][0]="2";
+        testobj[0][0][0][1][0][0]="3";
+        testobj[0][0][1][0][0][0]="4";
+        testobj[0][1][0][0][0][0]="5";
+        testobj[1][0][0][0][0][0]="6";
+	return testobj;
+    }
+}
+    
---
+++ tests.php4/array6.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+
+<?php
+if(!extension_loaded('java')) {
+  dl('java.' . PHP_SHLIB_SUFFIX);
+}
+
+$here=trim(`pwd`);
+java_set_library_path("$here/array6.jar");
+
+$testvar = new Java('Array6);
+$testobj = $testvar->test();
+echo $testobj[0][0][0][0][0][1];
+echo $testobj[0][0][0][0][1][0];
+echo $testobj[0][0][0][1][0][0];
+echo $testobj[0][0][1][0][0][0];
+echo $testobj[0][1][0][0][0][0];
+echo $testobj[1][0][0][0][0][0];
+?>
