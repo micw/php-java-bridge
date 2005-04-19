f261f76633f8fa8d681de2c11577a3a5158b7f1b
jost2345 <jost2345>
2005-04-19 20:32:26 +0000

    jpackage compatibility

--- server/php/java/bridge/JavaBridgeClassLoader.java
+++ server/php/java/bridge/JavaBridgeClassLoader.java
@@ -8,12 +8,17 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class JavaBridgeClassLoader extends URLClassLoader {
 	
     public JavaBridgeClassLoader() {
-    	super((URL[]) sysUrls.toArray(new URL[0]));
+        // Since 2.0.6 this is an empty list. jpackage requires that
+        // the user names the .jar file explicitly, for example via
+        // java_set_library_path("kawa-1.6.jar") or
+        // java_set_library_path("kawa.jar");
+	super(new URL[] {});	
     }
     // the local library directory (global one is /usr/share/java)
     static private String phpLibDir;
@@ -67,7 +72,11 @@ public class JavaBridgeClassLoader extends URLClassLoader {
     }
     //
     // add all jars found in the phpConfigDir/lib and /usr/share/java
-    // to our classpath
+    // to the list of our URLs.  The user is expected to name .jar
+    // libraries explicitly with java_set_library_path, e.g.
+    // java_set_library_path("foo.jar;bar.jar"); For backward
+    // compatibility we add all URLs we encountered during startup
+    // before throwing a "ClassNotFoundException".
     //
     static void initClassLoader(String phpConfigDir) {
         JavaBridgeClassLoader.phpLibDir=phpConfigDir + "/lib/";
@@ -92,7 +101,7 @@ public class JavaBridgeClassLoader extends URLClassLoader {
 			URL url;
 			file = "file:" + d.getAbsolutePath() + File.separator + file;
 			url = new URL(file);
-			Util.logMessage("added system library: " + url);
+			Util.logMessage("Found system library: "+ files[j] +" (url: " + url +").");
 			sysUrls.add(url);
 		    }  catch (MalformedURLException e1) {
 			Util.printStackTrace(e1);
@@ -104,4 +113,34 @@ public class JavaBridgeClassLoader extends URLClassLoader {
 	}
     }
 
+    // For backward compatibility.  Since 2.0.6 we don't add sysUrls
+    // automatically, the user is expected to reference them via
+    // e.g. java_set_library_path("foo-x.y.jar;bar-v.z.jar;...").  Before
+    // throwing a ClassNotFoundException we emulate the pre 2.0.6
+    // behaviour by adding the sysUrls ourselfs.
+    private void addSysUrls() {
+	for(Iterator e = sysUrls.iterator(); e.hasNext();) {
+	    addURL((URL)e.next());
+	}
+    }
+    protected Class findClass(String name) throws ClassNotFoundException { 
+	Class clazz=null;
+	try {
+	    clazz=super.findClass(name);
+	} catch (ClassNotFoundException e) {   
+	    Util.logMessage("Could not find class " + name + ". Adding all system libraries.  Please use java_set_library_path(\"...;<system library containing " + name + ">\") to avoid this message.");
+	    addSysUrls();
+	    clazz=super.findClass(name); 
+	}
+	return clazz;
+    }
+    public URL findResource(String name) {
+	URL url = super.findResource(name);
+	if(url==null) { 
+	    Util.logMessage("Could not find resource " + name + ". Adding all sysUrls.  Please use java_set_library_path(\"...;<system library containing " + name + ">\") to avoid this message.");
+	    addSysUrls();
+	    url = super.findResource(name);
+	}
+	return url;
+    }
 } 
