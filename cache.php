c8acde2430ab538e70547235daca9c2c64ae1ed9
jost2345 <jost2345>
2005-04-23 18:32:38 +0000

    *** empty log message ***

--- cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-//java_set_library_path("cache.jar");
-$Cache = new JavaClass("Cache");
-//$instance=$Cache->getInstance(); //instance will stay the VM until the VM runs out of memory
-echo $Cache->hashCode();
-echo $Cache->hashCode();
-?>
-
