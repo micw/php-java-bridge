e01048f18da09ad8f9abef6cf972985d053606ec
jost2345 <jost2345>
2005-04-28 17:01:59 +0000

    accept INET:0; search for a free port

--- server/php/java/bridge/TCPServerSocket.java
+++ server/php/java/bridge/TCPServerSocket.java
@@ -27,10 +27,22 @@ public class TCPServerSocket implements ISocketFactory {
 	new Listener(s.sock).listen();
 	return s;
     }
+
+    private static int findFreePort(int start) {
+	for (int port = start; port < start+100; port++) {
+	    try {
+		ServerSocket testSock = new ServerSocket(port);
+		testSock.close();
+	    } catch (IOException e) {continue;}
+	    return port;
+	}
+	return 0;
+    }
+
     private TCPServerSocket(int port, int backlog)
 	throws IOException {
-    	this.port=port;
-	this.sock=new ServerSocket(port, backlog);
+    	this.port = port==0?findFreePort(Integer.parseInt(DefaultSocketname)):port;
+	this.sock = new ServerSocket(port, backlog);
 	JavaBridge.initGlobals(null);
     }
 	
