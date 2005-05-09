8cc9fe86764fa510d07b6f1468f6cb75c43774bd
jost2345 <jost2345>
2005-05-09 21:12:04 +0000

    documentation update

--- update-about.sh
+++ update-about.sh
@@ -7,8 +7,7 @@ ed ABOUT.HTM <<\EOF
 1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/INSTALL?view=markup|INSTALL|
 1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/INSTALL\.WINDOWS?view=markup|INSTALL.WINDOWS|
 1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/PROTOCOL\.TXT?view=markup|PROTOCOL.TXT|
-1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/examples/clients/getProperties\.py?view=markup|examples/clients/getProperties.py|
-1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/examples/clients/listToArray\.scm?view=markup|examples/clients/listToArray.scm|
+1,$s|http://cvs\.sourceforge\.net/viewcvs\.py/php-java-bridge/php-java-bridge/examples/clients/README?view=markup|examples/clients/README|
 1,$s/^.*"SourceForge\.net Logo".*$//
 w
 q
