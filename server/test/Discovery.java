968f9ed50af480a5d09e8ab35378b9aefcc6d89f
jost2345 <jost2345>
2006-01-16 23:17:51 +0000

    *** empty log message ***

--- server/test/Discovery.java
+++ server/test/Discovery.java
@@ -5,11 +5,16 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class Discovery  {
- 	public static void main(String[] args) throws Exception {
-		ScriptEngineManager manager = new ScriptEngineManager();
-		ScriptEngine e = manager.getEngineByName("php");
-		e.put("hello", new StringBuffer("hello world"));
-		e.eval("<?php echo (string)java_context()->getAttribute('hello'); java_context()->setAttribute('hello', '!', 100);?>");
-		System.out.println(e.get("hello"));
-	}
+    public static void main(String[] args) throws Exception {
+	System.setProperty("php.java.bridge.default_log_level", "3");
+	System.setProperty("php.java.bridge.default_log_file", "");
+	System.setProperty("php.java.bridge.php_exec", "php-cgi");
+
+
+	ScriptEngineManager manager = new ScriptEngineManager();
+	ScriptEngine e = manager.getEngineByName("php");
+	e.put("hello", new StringBuffer("hello world"));
+	e.eval("<?php echo (string)java_context()->getAttribute('hello'); java_context()->setAttribute('hello', '!', 100);?>");
+	System.out.println(e.get("hello"));
+    }
 }
