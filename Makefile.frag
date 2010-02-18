8e89b0b1d2217f19cacab77e52c7ccd26291855d
jost_boekemeier <jost_boekemeier>
2010-02-18 17:22:12 +0000

    Release-6-0-0

--- Makefile.frag
+++
@@ -1,4 +0,0 @@
-# -*- mode: Makefile; -*-
-
-$(phplibdir)/stamp:
-	cd $(srcdir)/server; $(MAKE) CFLAGS="$(CFLAGS_CLEAN)" GCJFLAGS="$(GCJFLAGS) `echo $(CFLAGS_CLEAN)|sed 's/-D[^ ]*//g'`" install
--- test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Test script for Windows, type test.bat or double-click on test.bat
-rem and click "run".
-rem Then check RESULT.html and JavaBridge.log.
-@echo on
-java -classpath JavaBridge.war TestInstallation
-
-pause
