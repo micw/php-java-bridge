547781127dd43853f0e326e6439e90aaaa0094f3
jost_boekemeier <jost_boekemeier>
2010-07-03 17:59:09 +0000

    Release-6-2-1

--- server/php/java/bridge/http/HeaderParser.java
+++ server/php/java/bridge/http/HeaderParser.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
-import php.java.bridge.OutputStreamFactory;
 import php.java.bridge.Util;
 
 /*
