b1aab338f73b6f3250de7bf2c965f3476889fc99
jost_boekemeier <jost_boekemeier>
2010-07-04 11:00:06 +0000

    Release-6-2-1

--- server/php/java/bridge/test/AllTests.java
+++ server/php/java/bridge/test/AllTests.java
@@ -8,7 +8,8 @@ public class AllTests {
     public static Test suite() {
 	TestSuite suite = new TestSuite("Test for php.java.bridge.test");
 	//$JUnit-BEGIN$
-
+	suite.addTestSuite(TestInvocablePhpScriptEngine.class);
+	suite.addTestSuite(TestPhpScriptEngine.class);
 	//$JUnit-END$
 	return suite;
     }
